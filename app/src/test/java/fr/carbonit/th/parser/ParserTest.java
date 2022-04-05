4e110d319ad91fce039ad83c8820b94535c129a8
chukitipok <antonin.texier@gmail.com>
2022-04-05 22:04:10 +0200

    test(parser): verify that all test for invalid parser return InvalidParserException exception.

--- app/src/test/java/fr/carbonit/th/parser/ParserTest.java
+++ app/src/test/java/fr/carbonit/th/parser/ParserTest.java
@@ -1,10 +1,10 @@
 package fr.carbonit.th.parser;
 
 import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
@@ -12,12 +12,24 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class ParserTest {
 
+    private final File input;
+    private Parser parser;
+
+    public ParserTest() {
+        input = new File("src/test/resources/fixtures/parser/valid_input_test.txt");
+    }
+
+    @BeforeEach
+    public void init() {
+        parser = new Parser(input);
+    }
+
     @Test
     public void shouldAlertIfFileDoesNotExists() {
         var input = new File("src/test/resources/fixtures/parser/input.txt");
         ThrowingCallable parser = () -> new Parser(input);
 
-        assertThatExceptionOfType(FileNotFoundException.class).isThrownBy(parser);
+        assertThatExceptionOfType(InvalidParserException.class).isThrownBy(parser);
     }
 
     @Test
@@ -25,7 +37,7 @@ public class ParserTest {
         var input = new File("src/test/resources/fixtures/parser/empty_input_test.txt");
         ThrowingCallable parser = () -> new Parser(input);
 
-        assertThatExceptionOfType(FileIsEmptyException.class).isThrownBy(parser);
+        assertThatExceptionOfType(InvalidParserException.class).isThrownBy(parser);
     }
 
     @Test
@@ -33,7 +45,15 @@ public class ParserTest {
         var input = new File("src/test/resources/fixtures/parser/whitespace_input_test.txt");
         ThrowingCallable parser = () -> new Parser(input);
 
-        assertThatExceptionOfType(FileIsEmptyException.class).isThrownBy(parser);
+        assertThatExceptionOfType(InvalidParserException.class).isThrownBy(parser);
+    }
+
+    @Test
+    public void shouldAlertIfDetectIOException() {
+        var input = new File("src/test/resources/fixtures/parser/input.txt");
+        ThrowingCallable parser = () -> new Parser(input);
+
+        assertThatExceptionOfType(InvalidParserException.class).isThrownBy(parser);
     }
 
     @Test
