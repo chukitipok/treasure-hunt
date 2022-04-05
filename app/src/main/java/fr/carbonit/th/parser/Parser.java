2381a64eeeca4dc0b498f671614527e582722b6a
chukitipok <antonin.texier@gmail.com>
2022-04-05 21:07:55 +0200

    feat(parser): valid test shouldAlertIfFileHasEmptyContent

--- app/src/main/java/fr/carbonit/th/parser/Parser.java
+++ app/src/main/java/fr/carbonit/th/parser/Parser.java
@@ -2,12 +2,21 @@ package fr.carbonit.th.parser;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
 
 public class Parser {
 
-    public Parser(File input) throws FileNotFoundException {
+    public Parser(File input) throws IOException {
         if (!input.exists()) {
             throw new FileNotFoundException();
+        }
+
+        List<String> content = Files.readAllLines(input.toPath());
+
+        if (content.isEmpty()) {
+            throw new FileIsEmptyException();
         }
     }
 }
