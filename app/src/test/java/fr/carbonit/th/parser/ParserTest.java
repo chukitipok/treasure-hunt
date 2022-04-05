aa8ad28756fbdf776a56c69a76a134ad66a4e662
chukitipok <antonin.texier@gmail.com>
2022-04-05 20:43:31 +0200

    test(parser): test Parser class instantiation
    Verify that the file given exists to create Parser instance.

---
+++ app/src/test/java/fr/carbonit/th/parser/ParserTest.java
@@ -0,0 +1,21 @@
+package fr.carbonit.th.parser;
+
+import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+
+public class ParserTest {
+
+    @Test
+    public void shouldAlertIfFileDoesNotExists() {
+        var input = new File("src/test/resources/input.txt");
+        ThrowingCallable parser = () -> new Parser(input);
+
+        assertThatExceptionOfType(FileNotFoundException.class).isThrownBy(parser);
+    }
+}
