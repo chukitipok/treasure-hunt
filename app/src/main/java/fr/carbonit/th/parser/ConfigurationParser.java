7b4ca76d195310248ebc634c2ffb0ed542af3f50
chukitipok <antonin.texier@gmail.com>
2022-04-09 16:07:34 +0200

    test(parser): validate test shouldAlertIfRowContainsLessThan3ElementSeparatedByDashes

---
+++ app/src/main/java/fr/carbonit/th/parser/ConfigurationParser.java
@@ -0,0 +1,25 @@
+package fr.carbonit.th.parser;
+
+import fr.carbonit.th.reader.Reader;
+
+import java.io.File;
+import java.util.List;
+
+public class ConfigurationParser {
+
+    private final Reader reader;
+
+    public ConfigurationParser(Reader reader) {
+        this.reader = reader;
+    }
+
+    public void parse(File file) {
+        List<String> content = reader.read(file);
+        for (var row : content) {
+            var s = row.split("-");
+            if (s.length < 3) {
+                throw new UnparsableFileException();
+            }
+        }
+    }
+}
--- app/src/test/java/fr/carbonit/th/parser/ParserFileProvider.java
+++ app/src/test/java/fr/carbonit/th/parser/ParserFileProvider.java
@@ -7,6 +7,16 @@ import java.io.File;
 public class ParserFileProvider implements FileProvider<ParserFileType> {
 
     public File provide(ParserFileType fileType) {
-        return null;
+        File file;
+        switch (fileType) {
+            case ROW__WITH_LESS_THAN_3_ITEMS:
+                file = new File("src/test/resources/fixtures/parser/row_has_less_than_3_items.txt");
+                break;
+            default:
+                file = new File("src/test/resources/fixtures/parser/input.txt");
+                break;
+        }
+
+        return file;
     }
 }
