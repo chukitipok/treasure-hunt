40cfccbae134b0e46164d57938f006d1049be4e5
chukitipok <antonin.texier@gmail.com>
2022-04-10 19:20:52 +0200

    test(parser): should alert if treasure chest value is less than 1

--- app/src/test/java/fr/carbonit/th/command/TreasureCommandTest.java
+++ app/src/test/java/fr/carbonit/th/command/TreasureCommandTest.java
@@ -1,45 +1,53 @@
 package fr.carbonit.th.command;
 
-import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.ThrowableAssert.*;
 
 public class TreasureCommandTest {
 
     @Test
     public void shouldAlertIfItemsSizeDifferentThan4() {
-        ThrowableAssert.ThrowingCallable callable = () -> new TreasureCommand("X - X - X");
+        ThrowingCallable callable = () -> new TreasureCommand("X - X - X");
         assertThatExceptionOfType(InvalidCommandException.class).isThrownBy(callable);
     }
 
     @Test
     public void shouldAlertIfFirstItemIsNotEqualToLetterT() {
-        ThrowableAssert.ThrowingCallable callable = () -> new TreasureCommand("X - X - X - X");
+        ThrowingCallable callable = () -> new TreasureCommand("X - X - X - X");
         assertThatExceptionOfType(InvalidCommandException.class).isThrownBy(callable);
     }
 
     @Test
     public void shouldAlertIfSecondItemIsNotANumber() {
-        ThrowableAssert.ThrowingCallable callable = () -> new TreasureCommand("T - X - X - X");
+        ThrowingCallable callable = () -> new TreasureCommand("T - X - X - X");
         assertThatExceptionOfType(InvalidCommandException.class).isThrownBy(callable);
     }
 
     @Test
     public void shouldAlertIfThirdItemIsNotANumber() {
-        ThrowableAssert.ThrowingCallable callable = () -> new TreasureCommand("T - 1 - X - X");
+        ThrowingCallable callable = () -> new TreasureCommand("T - 1 - X - X");
         assertThatExceptionOfType(InvalidCommandException.class).isThrownBy(callable);
     }
 
     @Test
     public void shouldAlertIfFourthItemIsNotANumber() {
-        ThrowableAssert.ThrowingCallable callable = () -> new TreasureCommand("T - 1 - 1 - X");
+        ThrowingCallable callable = () -> new TreasureCommand("T - 1 - 1 - X");
         assertThatExceptionOfType(InvalidCommandException.class).isThrownBy(callable);
     }
 
     @Test
     public void shouldCreateTreasureCommandInstance() {
         assertThatCode(() -> new TreasureCommand("T - 1 - 1 - 2")).doesNotThrowAnyException();
+    }
+
+    @Test
+    public void shouldAlertIfLastItemValueIsLessThanOne() {
+        TreasureCommand command = new TreasureCommand("T - 1 - 1 - 0");
+        ThrowingCallable callable = command::handle;
+
+        assertThatExceptionOfType(InvalidCommandException.class).isThrownBy(callable);
     }
 }
