b1da939a1213912c7e5530ff744bd126e39d8532
chukitipok <antonin.texier@gmail.com>
2022-04-10 18:52:31 +0200

    test(parser): verify mountain has good properties from command line

--- app/src/main/java/fr/carbonit/th/configuration/Mountain.java
+++ app/src/main/java/fr/carbonit/th/configuration/Mountain.java
@@ -2,11 +2,16 @@ package fr.carbonit.th.configuration;
 
 import fr.carbonit.th.command.Coordinates;
 
+
 public class Mountain {
 
     private final Coordinates position;
 
     public Mountain(Coordinates position) {
         this.position = position;
+    }
+
+    public Coordinates getPosition() {
+        return position;
     }
 }
--- app/src/test/java/fr/carbonit/th/command/MountainCommandTest.java
+++ app/src/test/java/fr/carbonit/th/command/MountainCommandTest.java
@@ -1,10 +1,14 @@
 package fr.carbonit.th.command;
 
+import fr.carbonit.th.configuration.HuntMap;
+import fr.carbonit.th.configuration.Mountain;
+import fr.carbonit.th.configuration.exceptions.InvalidMapException;
 import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MountainCommandTest {
 
@@ -36,4 +40,30 @@ public class MountainCommandTest {
     public void shouldCreateMountainCommandInstance() {
         assertThatCode(() -> new MountainCommand("M - 1 - 1")).doesNotThrowAnyException();
     }
+
+    @Test
+    public void shouldCreateAnInstanceOfMountain() {
+        MountainCommand command = new MountainCommand("M - 1 - 1");
+        Mountain mountain = (Mountain) command.handle();
+
+        assertTrue(mountain.getPosition().getX().equals(1)
+                && mountain.getPosition().getY().equals(1));
+    }
+
+    @Test
+    public void shouldAlertIfRowNumberIsLessThanOne() {
+        MapCommand command = new MapCommand("C - 0 - 1");
+        ThrowableAssert.ThrowingCallable callable = command::handle;
+
+        assertThatExceptionOfType(InvalidMapException.class).isThrownBy(callable);
+    }
+
+    @Test
+    public void shouldAlertIfColumnNumberIsLessThanOne() {
+        MapCommand command = new MapCommand("C - 1 - 0");
+        ThrowableAssert.ThrowingCallable callable = command::handle;
+
+        assertThatExceptionOfType(InvalidMapException.class).isThrownBy(callable);
+    }
+
 }
