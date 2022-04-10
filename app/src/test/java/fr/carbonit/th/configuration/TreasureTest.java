ead4118164d9dae46a691ffa0f10699bfaf24e9d
chukitipok <antonin.texier@gmail.com>
2022-04-10 20:01:11 +0200

    test(parser): verify treasure x position is not less than 0

---
+++ app/src/test/java/fr/carbonit/th/configuration/TreasureTest.java
@@ -0,0 +1,20 @@
+package fr.carbonit.th.configuration;
+
+import fr.carbonit.th.command.Coordinates;
+import fr.carbonit.th.configuration.exceptions.InvalidTreasureException;
+import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+public class TreasureTest {
+
+    @Test
+    public void shouldAlertIfPositionXIsLessThanZero() {
+        Coordinates position = new Coordinates(-1, 0);
+        ThrowingCallable callable = () -> new Treasure(position, 3);
+
+        assertThatExceptionOfType(InvalidTreasureException.class).isThrownBy(callable);
+    }
+
+}
