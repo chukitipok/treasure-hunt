878427a6bab39e90772cbd0acd4410b7f8fd09a9
chukitipok <antonin.texier@gmail.com>
2022-04-10 18:43:23 +0200

    feat(parser): valid test shouldAlertIfRowNumberIsLessOrEqualThanZero

--- app/src/main/java/fr/carbonit/th/configuration/HuntMap.java
+++ app/src/main/java/fr/carbonit/th/configuration/HuntMap.java
@@ -1,11 +1,17 @@
 package fr.carbonit.th.configuration;
 
+import fr.carbonit.th.configuration.exceptions.InvalidMapException;
+
 public class HuntMap {
 
     private final Integer rows;
     private final Integer columns;
 
     public HuntMap(Integer rows, Integer columns) {
+        if (rows <= 0) {
+            throw new InvalidMapException();
+        }
+
         this.rows = rows;
         this.columns = columns;
     }
