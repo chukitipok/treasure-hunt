0d8ec59ca9241bba6308c343dc0387d1adb57cf4
chukitipok <antonin.texier@gmail.com>
2022-04-11 23:17:06 +0200

    feat(parser): valid test shouldLooseOneChest

--- app/src/main/java/fr/carbonit/th/simulation/Treasure.java
+++ app/src/main/java/fr/carbonit/th/simulation/Treasure.java
@@ -1,4 +1,18 @@
 package fr.carbonit.th.simulation;
 
 public class Treasure {
+
+    private Integer chests;
+
+    public Treasure() {
+        chests = 1;
+    }
+
+    public Integer getChests() {
+        return chests;
+    }
+
+    public void collected() {
+        chests = 0;
+    }
 }
