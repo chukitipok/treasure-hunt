f5164c0000383cf73dddf39e5dd5e87e6442fb3d
chukitipok <antonin.texier@gmail.com>
2022-04-12 00:48:38 +0200

    feat(orientation): valid test shouldHaveTheCorrectOrientationWhenTurningLeft

---
+++ app/src/main/java/fr/carbonit/th/simulation/Orientation.java
@@ -0,0 +1,23 @@
+package fr.carbonit.th.simulation;
+
+public enum Orientation {
+
+    NORTH(0, 1),
+    EAST(1, 0),
+    SOUTH(0, -1),
+    WEST(-1, 0);
+
+    private final int offsetX;
+    private final int offsetY;
+
+    Orientation(final int offsetX, final int offsetY) {
+        this.offsetX = offsetX;
+        this.offsetY = offsetY;
+    }
+
+    public Orientation left() {
+        if (this == NORTH)
+            return WEST;
+        return Orientation.values()[this.ordinal() - 1];
+    }
+}
