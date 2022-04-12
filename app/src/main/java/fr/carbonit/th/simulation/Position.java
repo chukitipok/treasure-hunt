5307f20f4c2cfd169c5911404c02a372df0a6e1a
chukitipok <antonin.texier@gmail.com>
2022-04-12 14:47:19 +0200

    feat(position): valid test shouldTurnLeft

--- app/src/main/java/fr/carbonit/th/simulation/Position.java
+++ app/src/main/java/fr/carbonit/th/simulation/Position.java
@@ -1,5 +1,7 @@
 package fr.carbonit.th.simulation;
 
+import java.util.Objects;
+
 public class Position {
 
     private final Orientation orientation;
@@ -11,6 +13,17 @@ public class Position {
     }
 
     public Position left() {
-        return null;
+        return new Position(orientation.left(), coordinates);
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return orientation == position.orientation && Objects.equals(coordinates, position.coordinates);
+    }
+
+    public int hashCode() {
+        return Objects.hash(orientation, coordinates);
     }
 }
