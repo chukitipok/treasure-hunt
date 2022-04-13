4c04073a1f2b1b588b37feefe6611e041f4f4a6d
chukitipok <antonin.texier@gmail.com>
2022-04-13 03:16:57 +0200

    test(application): verify that method argument is a valid file

--- app/src/test/java/fr/carbonit/th/ApplicationTest.java
+++ app/src/test/java/fr/carbonit/th/ApplicationTest.java
@@ -1,6 +1,12 @@
 package fr.carbonit.th;
 
+import fr.carbonit.th.parser.UnparsableFileException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.assertj.core.api.ThrowableAssert.*;
@@ -17,5 +23,20 @@ public class ApplicationTest {
     public void shouldAlertIfArgsAreMoreThanOne() {
         ThrowingCallable callable = () -> Application.main(new String[2]);
         assertThatExceptionOfType(TooManyParametersException.class).isThrownBy(callable);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideInvalidFilePaths")
+    public void shouldAlertIfFileIsNotValid(String[] args) {
+        ThrowingCallable callable = () -> Application.main(args);
+        assertThatExceptionOfType(UnparsableFileException.class).isThrownBy(callable);
+    }
+
+    private static Stream<Arguments> provideInvalidFilePaths() {
+        return Stream.of(
+                Arguments.of((Object) new String[]{""}),
+                Arguments.of((Object) new String[]{"john/doe"}),
+                Arguments.of((Object) new String[]{"john.doe"})
+        );
     }
 }
