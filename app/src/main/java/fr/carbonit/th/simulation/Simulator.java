506b42db661a2ca34155e4366fe67a176183d784
chukitipok <antonin.texier@gmail.com>
2022-04-13 15:57:27 +0200

    test(map): verify that adventurer should have collected 3 treasures

---
+++ app/src/main/java/fr/carbonit/th/simulation/Simulator.java
@@ -0,0 +1,8 @@
+package fr.carbonit.th.simulation;
+
+public class Simulator {
+
+    public TreasureMap simulate(TreasureMap configuration) {
+        return null;
+    }
+}
---
+++ app/src/test/java/fr/carbonit/th/simulation/SimulatorTest.java
@@ -0,0 +1,30 @@
+package fr.carbonit.th.simulation;
+
+import fr.carbonit.th.configuration.TreasureHuntConfiguration;
+import fr.carbonit.th.provider.ConfigurationProvider;
+import fr.carbonit.th.simulation.mappers.TreasureMapMapper;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimulatorTest {
+
+    private final ConfigurationProvider provider;
+    private final Simulator simulator;
+    private final TreasureMapMapper mapper;
+
+    public SimulatorTest() {
+        simulator = new Simulator();
+        provider = new ConfigurationProvider();
+        mapper = new TreasureMapMapper();
+    }
+
+    @Test
+    public void shouldAdventurerHaveCollected3Treasures() {
+        TreasureHuntConfiguration configuration = provider.provide();
+        TreasureMap map = simulator.simulate(mapper.map(configuration));
+        Adventurer adventurer = map.getAdventurers().get(0);
+
+        assertEquals(3, adventurer.getTreasures());
+    }
+}
